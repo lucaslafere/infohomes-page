2ec2a9d5989c866432e209ebf8287992d9f83c29
Lucas Lafèré <laferelucas@gmail.com>
2023-02-08 16:33:49 -0300

    feat: added hover to 'how' button

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import { ThemeProvider } from "styled-components";
 import { theme } from "./theme/theme";
-import Subscribe from "./components/buttons/Subscribe";
 import How from "./components/buttons/How";
 
 export default function App() {
   return (
     <ThemeProvider theme={theme}>
-      <Subscribe />
       <How />
     </ThemeProvider>
   );
--- src/components/buttons/How/style.js
+++ src/components/buttons/How/style.js
@@ -5,10 +5,12 @@ export const Button = styled.button`
   justify-content: center;
   align-items: center;
   padding: 9px 24px;
-  width: 162px;
+  width: 170px;
   height: 42px;
   border: 1px solid ${({ theme }) => theme.Green25};
   border-radius: 4px;
+  background-color: #fff;
+  cursor: pointer;
 
   font-family: "Poppins";
   font-style: normal;
@@ -16,4 +18,8 @@ export const Button = styled.button`
   font-size: 16px;
   line-height: 24px;
   color: ${({ theme }) => theme.Green100};
+
+  :hover {
+    background-color: ${({ theme }) => theme.Green25};
+  }
 `;
