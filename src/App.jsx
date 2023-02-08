d0b61abd8aa462fccfe516e6fa030c114f549c5c
Lucas Lafèré <laferelucas@gmail.com>
2023-02-08 16:41:56 -0300

    removed community from app

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,6 @@
 import { ThemeProvider } from "styled-components";
-import Community from "./components/buttons/Community";
 import { theme } from "./theme/theme";
 
 export default function App() {
-  return (
-    <ThemeProvider theme={theme}>
-      <Community>Top Opportunities</Community>
-      <Community>Data Health</Community>
-    </ThemeProvider>
-  );
+  return <ThemeProvider theme={theme}></ThemeProvider>;
 }
