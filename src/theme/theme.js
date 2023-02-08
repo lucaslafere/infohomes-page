0421c93c6f1df2a4f20763120049740bd0c619f3
Lucas Lafèré <laferelucas@gmail.com>
2023-02-08 15:58:56 -0300

    added gray themes

---
+++ src/theme/theme.js
@@ -0,0 +1,10 @@
+export const theme = {
+  Gray100: "#162219",
+  Gray75: "#464B53",
+  Gray50: "#6D7178",
+  Gray25: "#A9B1BC",
+  Gray15: "#D1D5DB",
+  Gray10: "#E9EAEC",
+  Gray5: "#FAFAFA",
+  Gray0: "#FFF",
+};
