f1b38e3e1b20d794855e245340bbdd0d14d76872
Lucas Lafèré <laferelucas@gmail.com>
2023-02-10 12:10:41 -0300

    changed community to anchor

--- src/components/buttons/Community/style.js
+++ src/components/buttons/Community/style.js
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-export const Community = styled.div`
+export const Community = styled.a`
   display: flex;
   align-items: center;
   justify-content: center;
