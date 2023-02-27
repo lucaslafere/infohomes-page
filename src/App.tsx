a1377271585545486ebbedde76b7de1946e7c695
Lucas Lafèré <laferelucas@gmail.com>
2023-02-27 15:34:03 -0300

    fixed globalstyle errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import { ThemeProvider } from "styled-components";
-import { theme } from "./theme/theme";
+import GlobalStyle from "./assets/styles/global";
 import { Community } from "./components/buttons/Community";
+import { theme } from "./theme/theme";
 
 export default function App() {
-  return <ThemeProvider theme={theme}></ThemeProvider>;
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+    </ThemeProvider>
+  );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,8 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 
 import "./assets/styles/reset.css";
-import "./assets/styles/style.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
