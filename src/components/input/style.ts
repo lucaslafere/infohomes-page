f66d7157dee6fbf0edf6d55a438a6b26580ee038
Lucas Lafèré <laferelucas@gmail.com>
2023-03-06 17:02:59 -0300

    better input styling

--- src/components/input/style.ts
+++ src/components/input/style.ts
@@ -1,19 +1,32 @@
 import styled from "styled-components";
 
-//create an input component styled with styled-components and export it
 export const Input = styled.input`
   display: flex;
   align-items: center;
   justify-content: center;
+
   gap: 10px;
   padding: 9px 13px;
+
   background: ${({ theme }) => theme.Gray[0]};
   border: 1px solid ${({ theme }) => theme.Gray[15]};
   box-shadow: 0px 15px 16px -8px rgba(149, 149, 149, 0.1);
   border-radius: 4px;
-`;
-//create an icon component styled with styled-components and export it
-export const Icon = styled.img`
-  width: 20px;
-  height: 20px;
+
+  font-family: "Open Sans";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 24px;
+
+  :hover {
+    border: 1px solid ${({ theme }) => theme.Gray[25]};
+  }
+  :focus {
+    border: 1px solid ${({ theme }) => theme.Green[75]};
+    outline: none;
+  }
+  ::placeholder {
+    color: ${({ theme }) => theme.Gray[25]};
+  }
 `;
