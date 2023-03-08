911c20e4052e2b8cc1a8e3e1c187478b1d862800
Lucas Lafèré <laferelucas@gmail.com>
2023-03-08 12:34:19 -0300

    fix: footer flex start

--- src/layouts/Footer/style.ts
+++ src/layouts/Footer/style.ts
@@ -3,8 +3,8 @@ import styled from "styled-components";
 export const FooterContainer = styled.footer`
   display: flex;
   justify-content: center;
+  align-items: flex-start;
   min-height: 400px;
-  align-items: center;
   background: ${({ theme }) => theme.Gray[5]};
   border-top: 1px solid ${({ theme }) => theme.Gray[10]};
 `;
